4f002b92da5c428499dc7392de4daecfc22ef1bf
vamsipolineni <imvamshik18@gmail.com>
2025-07-09 21:07:16 +0530

    fourth commit

--- projects/drag_and_drop.py
+++ projects/drag_and_drop.py
@@ -9,36 +9,21 @@ frame_ele = "//iframe[@class='demo-frame lazyloaded']"
 
 driver = webdriver.Chrome()
 try:
-    # url = "https://www.globalsqa.com/demo-site/draganddrop/"
-    # print("navigating to webpage...")
-    # driver.get(url)
-    # print("maximizing window....")
-    # driver.maximize_window()
-    # print("switching to frame....")
-    # frame = driver.find_element(By.XPATH, frame_ele)
-    # driver.switch_to.frame(frame)
-    # source_ele = driver.find_element(By.XPATH, source_path)
-    # target_ele = driver.find_element(By.XPATH, target_path)
-    # time.sleep(2)
-    # print("performing drag and drop....")
-    # action = ActionChains(driver)
-    # action.drag_and_drop(source_ele, target_ele).perform()
-    # print("dragged and dropped successfully....")
-    # time.sleep(4)
-    url = "https://formy-project.herokuapp.com/dragdrop"
+    url = "https://www.globalsqa.com/demo-site/draganddrop/"
     print("navigating to webpage...")
     driver.get(url)
     print("maximizing window....")
     driver.maximize_window()
-    source_ele = driver.find_element(By.ID, 'image')
-    target_ele = driver.find_element(By.ID, 'box')
+    print("switching to frame....")
+    frame = driver.find_element(By.XPATH, frame_ele)
+    driver.switch_to.frame(frame)
+    source_ele = driver.find_element(By.XPATH, source_path)
+    target_ele = driver.find_element(By.XPATH, target_path)
     time.sleep(2)
-    action = ActionChains(driver)
     print("performing drag and drop....")
+    action = ActionChains(driver)
     action.drag_and_drop(source_ele, target_ele).perform()
-    time.sleep(5)
     print("dragged and dropped successfully....")
-except Exception as e:
-    print(e)
-finally:
-    driver.quit()
+    time.sleep(4)
+except Exception as es:
+    print(es)
